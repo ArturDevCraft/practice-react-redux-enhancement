20a86440f02b7fe923d27893fbc86b3129bf60d8
ArturDevCraft <artur.czerwiak@gmail.com>
2026-03-15 19:35:26 +0100

    Task 02 completed

--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (opacity = 1.0, background = 'none') => {
+	return (WrappedComponent) => {
+		return () => {
+			const dialog = useRef();
+			const [opened, setOpened] = useState(true);
+			useEffect(() => {
+				opened ? dialog.current.showModal() : dialog.current.close();
+			}, [opened]);
 
+			const closeHandler = () => {
+				setOpened(false);
+			};
+
+			const dialogStyles = { backgroundColor: background, opacity: opacity };
+			const wrapperStyles = { display: 'grid' };
+
+			return (
+				<>
+					<dialog ref={dialog} style={dialogStyles}>
+						<div style={wrapperStyles}>
+							<WrappedComponent />
+							<button onClick={closeHandler}>Ok</button>
+						</div>
+					</dialog>
+
+					{!opened && <WrappedComponent />}
+				</>
+			);
+		};
+	};
+};
 
 export default withPopup;
